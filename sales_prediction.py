5904204c5883daa74237225cb3b81fbde45e85e8
nath-asha <102415462+nath-asha@users.noreply.github.com>
2024-02-21 10:32:05 +0530

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -8,3 +8,5 @@ dataset.shape
 dataset.isna().sum() '''check for missing values'''
 dataset.duplicated().any() '''check for duplicate values'''
 fig,axs = plt.subplots(2,figsize=(5,5))
+plt1 = sns.boxplot(dataset['TV'],ax = axs[0])
+plt2 = sns.boxpot(dataset['Newspaper'],ax = axs[1])
